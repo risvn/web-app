c3161fecc08811833999b03f048c4e3d8d3f4e2b
rsvn <rizwanshaz2004@gmail.com>
2025-08-16 21:16:41 +0530

    <database , home-page>

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
+from .models import Post
 
-# Create your views here.
+
+from django.shortcuts import render
+
+
+
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts':Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request,'blog/about.html')
-
-
-
+    return render(request, 'blog/about.html', {'title': 'About'})
