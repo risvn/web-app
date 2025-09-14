2e9155b4a182cdaf1b8d750fe383eada5a4ee43c
rsvn <rizwanshaz2004@gmail.com>
2025-09-14 23:23:23 +0530

    UPDATE:usr-profile(user profile image)

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,8 @@ urlpatterns = [
     path('profile/',user_views.profile,name='profile'),
     path('logout/', user_views.logout_view,name='logout'),
 ]
+
+## use this only in development server check out official doucmentation for the 
+## deployment
+if settings.DEBUG:
+    urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
