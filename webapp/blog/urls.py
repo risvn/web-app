330056d7486d0345d11ff64a66be56ebc939eaaf
rsvn <rizwanshaz2004@gmail.com>
2025-09-30 23:29:42 +0530

    ADD(crud-ops):create,delete,update posts

--- webapp/blog/urls.py
+++ webapp/blog/urls.py
@@ -5,6 +5,9 @@ from .views import PostListView,PostDetailView
 
 urlpatterns=[
         path('',PostListView.as_view(),name='blog-home'),
-    path('post/<int:pk>',PostDetailView.as_view(),name='post-detail'),
+        path('post/<int:pk>',PostDetailView.as_view(),name='post-detail'),
+        path('update/<int:pk>',views.post_update,name='post-update'),
+        path('delete/<int:pk>',views.post_delete,name='post-delete'),
         path('about/',views.about,name='blog-about'),
+        path('create/',views.post_create,name='post-create'),
 ]
--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from django import forms
 from .models import Post
 from django.views.generic import ListView,DetailView
-
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
 
@@ -19,6 +19,44 @@ def home(request):
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
+@login_required
+def post_create(request):
+    if request.method=="POST":
+        title=request.POST.get("title")
+        content=request.POST.get("content")
+        author=request.user
+
+
+        Post.objects.create(title=title, content=content,author=author)
+
+        return redirect("blog-home")
+
+
+    return render(request, "blog/post_create.html")
+
+
+@login_required
+def post_update(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+
+    if request.method=="POST":
+        post.title=request.POST.get("title")
+        post.content=request.POST.get("content")
+        post.save()
+        return redirect("blog-home")
+
+    return render(request, "blog/post_update.html", {"post": post})
+
+
+
+@login_required
+def post_delete(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+
+    if request.method=="GET":
+       post.delete()
+       return redirect("blog-home")
+    return redirect("blog-home")
 
 class PostListView(ListView):
     model=Post
